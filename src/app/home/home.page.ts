eabf81b770608cb930fc159148e3813150a49c10
Dryzin <endrioemanuel11@gmail.com>
2022-10-27 08:59:26 -0300

    imagens e função

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,20 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Guid } from 'guid-typescript';
+import { Pessoa } from '../models/pessoa.model';
+import { FormGroup } from '@angular/forms';
+import { PessoasService } from '../services/pessoas.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
+  private pessoa : Pessoa;
+  private pessoaForm : FormGroup;
+  public arrayPessoa: any
+
+
+
+  id: Guid
   alt : number
   peso : number
   resultado: string
   inf: string
   gen: string
-  imagem: any = "assets/icon/favicon.png"
+  imagem: any = "assets/icon/balanca.png"
 
-  constructor() {}
+  constructor(
+    private pessoaService : PessoasService 
+  ) {}
+
+  ngOnInit(){}
+
+  enviar(){
+    if (this.pessoaForm.valid){
+      this.pessoaService.inserir(this.pessoaForm.value)
+    }}
 
   IMC(){
 
@@ -43,12 +64,12 @@ export class HomePage {
 
       if (conta <= 20.7){
         this.inf = "Abaixo do Peso"
-        this.imagem = "assets/icon/favicon.png"
+        this.imagem = "assets/icon/balanca.png"
       }
   
       else if (conta > 20.7 && conta <= 27.8){
         this.inf = "Peso Normal"
-        this.imagem = "assets/icon/favicon.png"
+        this.imagem = "assets/icon/balanca.png"
       }    
   
       else {
@@ -58,8 +79,16 @@ export class HomePage {
       
     }
 
-    this.resultado = conta.toFixed(2)
+      this.resultado = conta.toFixed(2)
+
+    this.pessoa = {
+    id: Guid.createEmpty(),
+    alt : this.alt.toString(),
+    peso : this.peso.toString(),
+    resultado: this.resultado.toString(),
+    inf: this.inf,
+    gen: this.gen
+    }
+    this.pessoaService.inserir(this.pessoa)
   }
-
-
 }
